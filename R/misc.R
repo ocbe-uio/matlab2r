0feeb3bf3321b68c9eee233f6f52170befb704d1
Waldir Leoncio <w.l.netto@medisin.uio.no>
2022-05-13 14:47:29 +0200

    Added package load notice that closes #13

---
+++ R/misc.R
@@ -0,0 +1,19 @@
+# This file contains miscellaneous functions that are not to be used by a package user.
+
+#' @title Prints welcome message on package load
+#' @description Prints package version number and welcome message on package load
+#' @param libname library location. See `?base::.onAttach` for details
+#' @param pkgname package name. See `?base::.onAttach` for details
+.onAttach <- function(libname, pkgname) {
+  version <- read.dcf(
+    file   = system.file("DESCRIPTION", package = pkgname),
+    fields = "Version"
+  )
+  packageStartupMessage(
+    "This is ", paste(pkgname, version), "\n\n",
+    "Please notice that some of this package functions (listed above) have ",
+    "homonymous functions on other packages. Therefore, you should consider ",
+    "making explicit calls to them in the form 'package::function()' to avoid ",
+    "ambiguous calls."
+  )
+}
