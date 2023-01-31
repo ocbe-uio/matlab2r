b58c2a46108822e1bcda19b823d600376fa4e874
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-31 12:19:49 +0100

    Added methods covering remaining examples (#18)

--- R/ismember.R
+++ R/ismember.R
@@ -5,7 +5,9 @@
 #' @param rows if \code{TRUE}, each row of A and each row of B are treated as
 #' single entities
 #' @param indices if \code{TRUE}, outputs the lowest B index for each match in A
-#' @return
+#' @return a binary vector telling if the corresponding A indices are in B. If
+#' \code{indices = TRUE}, also prints the index in B where the match first
+#' occurs.
 #' @author Waldir Leoncio
 #' @export
 #' @examples
@@ -26,12 +28,42 @@
 #' )
 #' ismember(A, B)
 #'
-ismember <- function(A, B, rows = FALSE, indices = FALSE) {
-  if (is(A, "numeric") && is(B, "numeric")) {
-    matches <- match(A, B)
-    out <- as.numeric(!is.na(matches))
-    matches[is.na(matches)] <- 0
+setGeneric(
+  name = "ismember",
+  def  = function(A, B, rows = FALSE, indices = FALSE) {
+    if (rows) {
+      A <- as.data.frame(A)
+      B <- as.data.frame(B)
+      return(ismember(A, B, rows, indices))
+    }
+    matches <- match(A, B, nomatch = 0L, incomparables = NaN)
+    out <- as.integer(matches != 0L)
+    if (is(A, "matrix")) out <- matrix(out, nrow = nrow(A))
     if(indices) out <- list("presence" = out, "locations" = matches)
+    return(out)
   }
-  return(out)
+)
+setMethod(
+  f = "ismember",
+  signature = c("data.frame", "data.frame"),
+  definition = function(A, B, rows, indices) {
+    out <- vector("integer", length = nrow(A))
+    matches <- vector("integer", length = nrow(A))
+    for (a in seq_len(nrow(A))) {
+      for (b in seq_len(nrow(B))) {
+        if (all(A[a, ] == B[b, ])) {
+          out[a] <- 1L
+          matches[a] <- ifelse(matches[a] == 0, b, base::min(b, matches[a]))
+        }
+      }
+    }
+    if(indices) out <- list("presence" = out, "locations" = matches)
+    return(out)
+  }
+)
+
+ismembertol <- function(A, B, rows = FALSE, indices = TRUE) {
+  A <- round(A, digits = 10L)
+  B <- round(B, digits = 10L)
+  ismember(A, B)
 }
