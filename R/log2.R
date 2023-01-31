ec0ed85aba3e2cf7aad62d33297b14930a30e0cb
Waldir Leoncio <w.l.netto@medisin.uio.no>
2023-01-31 07:12:33 +0100

    Algebraic dissection (fixes #17)
    Credit for the solution goes to Rohit on StackOverflow (https://stackoverflow.com/a/50833387/1169233).

--- R/log2.R
+++ R/log2.R
@@ -1,30 +1,22 @@
 #' @title Base 2 logarithm
 #' @description Base 2 logarithm and floating-point number dissection
 #' @param X a scalar or vector of numbers
-#' @param tol a tolerance level for numerical calculation of the log
-#' @note User, beware: this is ridiculously slow and written in a hurry. If you
-#' simply want to compute the base 2 logarithm of a number, use \code{log2()}
-#' from \code{base}.
-#' @return a list of mantissas and exponents such that mantissa * 2 ^ exponent
-#' equals X
+#' @param dissect if \code{TRUE}, returns the mantissa and exponent.
+#' @return either a vector or a list of mantissas and exponents such that
+#' mantissa * 2 ^ exponent equals X
 #' @export
-log2 <- function(X, tol = 1e-4) {
+log2 <- function(X, dissect = TRUE) {
+  if (!dissect) return(base::log2(X))
   me <- data.frame(row.names = X)
   for (x in X) {
     if (x == 0) {
       me[row.names(me) == x, "mantissa"] <- 0
       me[row.names(me) == x, "exponent"] <- 0
     } else {
-      for (mantissa in seq(-1, 1, .00001)) {
-        for (exponent in seq(-100, 100)) {
-          operation <- mantissa * 2 ^ exponent
-          diff <- abs(operation - x)
-          if (diff <= tol) {
-            me[row.names(me) == x, "mantissa"] <- mantissa
-            me[row.names(me) == x, "exponent"] <- exponent
-          }
-        }
-      }
+      E <- ifelse(x == 0, 0, floor(base::log2(abs(x))) + 1)
+      F <- x / 2 ^ E
+      me[row.names(me) == x, "mantissa"] <- F
+      me[row.names(me) == x, "exponent"] <- E
     }
   }
   return(as.list(me))
